90a4e245a4c7563a2acefc34cb9f52ce2dc40c1a
hamzah olaniyan <hamzaholaniyan88@gmail.com>
2026-02-03 09:55:41 +0000

    favicon

--- src/components/ui/logo/index.tsx
+++ src/components/ui/logo/index.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import Image from "next/image";
 import logo from "../../../../public/logo.png";
+import kcg from "../../../../public/kcg1.svg";
+
 export default function Logo({ size = 120 }: { size?: number }) {
    return (
       <Link href="/" className="flex items-center justify-center gap-2 absolute top-0 p-2">
@@ -13,9 +15,7 @@ export default function Logo({ size = 120 }: { size?: number }) {
             className="rounded-full border-6 border-white bg-white"
          />
          <div className="">
-            <p className="hidden lg:flex text-theme_green font-extrabold text-2xl text-justify">
-               The Kennel Club Gambia
-            </p>
+            <Image src={kcg} alt={"logo"} width={200} height={200} priority className="text-red-600 fill-amber-700" />
          </div>
       </Link>
    );
